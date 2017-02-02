e8fd1adf88496409abf1d5210b30bfc02992c4a0
ShivamKumarJha <jha.shivam3@gmail.com>
2017-02-02 10:12:48 +0530

    cancro: use the old start_sensors() method
    Pesonally for me sensors are working great after update too.
    But for some people using Mi3W even after updating to latest MIUI, sensors do not work.
    So revert back to the old way for them.

--- rootdir/root/init.qcom.sh
+++ rootdir/root/init.qcom.sh
@@ -38,19 +38,23 @@ fi
 start_sensors()
 {
     if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
+        mkdir -p /data/system/sensors		
+        chown -h system.system /data/system/sensors		
+        restorecon -r /data/system/sensors		
+        touch /data/system/sensors/settings		
+        chmod -h 775 /data/system/sensors		
+        chmod -h 664 /data/system/sensors/settings		
+        chown -h system /data/system/sensors/settings
+    
         mkdir -p /data/misc/sensors
         chmod -h 775 /data/misc/sensors
         mkdir -p /persist/misc/sensors
         chmod 775 /persist/misc/sensors
 
-        touch /persist/misc/sensors/settings
-        chmod -h 664 /data/system/sensors/settings
-        chown -h system /persist/misc/sensors/settings
-
-        if [ ! -s /persist/misc/sensors/settings ]; then
+        if [ ! -s /data/system/sensors/settings ]; then
             # If the settings file is empty, enable sensors HAL
             # Otherwise leave the file with it's current contents
-            echo 1 > /persist/misc/sensors/settings
+            echo 1 > /data/system/sensors/settings
         fi
         start sensors
     fi
