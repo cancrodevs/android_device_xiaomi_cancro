382a057c8538b1b56379b88da51c376cc1b91053
KhabarlakKonstantin <habarlack@gmail.com>
2017-09-19 19:25:51 +0300

    cancro: Compress boot and recovery ram image
    Fixes image too large error
    
    Change-Id: I242c53addff4d6e4c54e85c93bc592b120f9b0a6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ BOARD_KERNEL_SEPARATED_DT          := true
 BOARD_KERNEL_BASE                  := 0x00000000
 BOARD_KERNEL_PAGESIZE              := 2048
 BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
+LZMA_RAMDISK_TARGETS               := boot,recovery
 TARGET_KERNEL_SOURCE               := kernel/xiaomi/cancro
 TARGET_KERNEL_ARCH                 := arm
 TARGET_KERNEL_CONFIG               := lineageos_cancro_defconfig
