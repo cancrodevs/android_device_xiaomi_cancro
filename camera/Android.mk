5e4006d4ef5fa6ce9c22755abb980595f39fddf1
Bruno Martins <bgcngm@gmail.com>
2017-12-01 17:00:59 +0530

    cancro: CameraWrapper: Consider device custom headers
     * Again, for some reason the custom CameraParameters header
       was not being taken into account without explicitly include it,
       causing the whole camera stack to crash.
    
    Change-Id: I5cb97d4ae26db482f28d277e8ec63420eb389b19

--- camera/Android.mk
+++ camera/Android.mk
@@ -6,12 +6,13 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../include \
     frameworks/native/include/media/openmax \
     system/media/camera/include
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libsensor \
+    libhardware liblog libcamera_client libutils libcutils libsensor libdl\
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
 
