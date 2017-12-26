a9947d37bbd40f1f4863c803f234f1a997e98b28
DrRamm <kotsareu@gmail.com>
2017-12-26 15:35:51 +0300

    fix boot on mi3
    Change-Id: I4e4fb50ab53e56875a4077d5a672d6c8d0043c0e

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -39,6 +39,7 @@ else
     # Remove consumerir support
     rm -rf /system/etc/permissions/android.hardware.consumerir.xml
     rm -rf /system/lib/hw/consumerir.msm8974.so
+    rm -rf system/vendor/lib/hw/android.hardware.ir@1.0-impl.so
     # Remove Mi4 audio configs
     rm -rf /system/etc/acdbdata/MTP/MTP_Speaker_cal_4.acdb
     rm -f /system/etc/mixer_paths_4.xml
