2bace969e14f9cc719a6de04aa5b60a9d061f1f5
ShivamKumarJha <jha.shivam3@gmail.com>
2017-12-27 13:16:10 +0530

    cancro: Build camera hidl

--- cancro.mk
+++ cancro.mk
@@ -48,6 +48,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     camera.msm8974 \
     libxml2 \
     Snap
