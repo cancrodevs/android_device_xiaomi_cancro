52d9096e306835a24f31883b75838b69f51c96b4
ShivamKumarJha <jha.shivam3@gmail.com>
2017-12-27 13:22:18 +0530

    cancro: Remove keymaster service hidl

--- cancro.mk
+++ cancro.mk
@@ -239,8 +239,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
- android.hardware.keymaster@3.0-impl \
- android.hardware.keymaster@3.0-service
+ android.hardware.keymaster@3.0-impl
 
 # Keystore
 PRODUCT_PACKAGES += \
